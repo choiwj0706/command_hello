311d3cbce5347c5240c601e6ccecfa0cb011c05a
choiwj0706 <63217136+choiwj0706@users.noreply.github.com>
2020-04-08 09:45:20 +0900

    GitHub for git pull

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 // For command line git tutorial
 // Local repository modification
-
+// GitHub modification
 print("Hello world")
 print("Tell your world")
 printf("Tell his world")
-
 print("Tell her world")
+print("Tell my world")
